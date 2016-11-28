881af66c4547f2c01fbdcfd7bd65c62748467757
Innerbytes <info@innerbytes.com>
2016-11-28 22:38:43 +0100

    Added simple menu and coroutine manager.

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using MinDI;
+
+public class GameMenu : ContextMonoBehaviour {
+
+	[Injection] ICoroutineManager coroutines { get; set; }
+
+	protected override void OnInjected ()
+	{
+		Debug.Log("Game menu is now onjected!");
+	}
+
+	void OnGUI() {
+		GUILayout.Label ("Hello world");
+		GUILayout.Label ("Coroutines manager is: "+coroutines);
+	}
+}
